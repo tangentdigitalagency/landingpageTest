96e7d31dcf58810602328d613bf60ceb4bf28a96
Hassan <hassanali.naqvi@ajsoftware.com.pk>
2021-03-19 00:37:05 +0500

    handled two section services and policy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
-import { Row, Col,Layout,Button} from "antd";
+import {Button, Col, Layout, Row} from "antd";
 import logo from './assets/img/logo.png';
+import farmers from './assets/img/farmers.png';
+import progressive from './assets/img/progressive.png';
+import travelers from './assets/img/travelers.png';
+import stateAuto from './assets/img/state-auto.png';
 import heroImg from './assets/img/hero-img.jpg';
-import { ArrowRightOutlined} from '@ant-design/icons';
+import {ArrowRightOutlined} from '@ant-design/icons';
+
 function App() {
     const {Header, Footer, Content} = Layout;
 
@@ -11,14 +16,14 @@ function App() {
         <Layout>
           <Header>
              <Row>
-                  <Col  xs={{ span: 12 }} lg={{ span: 12 }}>
-                      <a href="#"><img src={logo} width="181" alt='logo'/></a>
+                  <Col xs={{span: 12}} lg={{span: 12}}>
+                      <a href="#"><img src={logo} width="181" alt="logo"/></a>
                   </Col>
-                  <Col xs={{ span: 12 }} lg={{ span: 12 }}><strong>Call For Free Quote</strong></Col>
+                  <Col xs={{span: 12}} lg={{span: 12}}><strong>Call For Free Quote</strong></Col>
              </Row>
           </Header>
           <Content>
-              <section className="section-one" >
+              <section className="section-one">
                 <Row>
                   <Col span={24}>
                       <div className="container-1">
@@ -28,7 +33,7 @@ function App() {
                                  <h1 className="hero_heading">Better Coverage with Quantum. Get a free Commercial Insurance Quote</h1>
                                  <p className="text-white para-white">A business owners policy includes protection for all major property and liability risk in one package. Offers protection to business owners against property damage, peril, business interruption, and liability. Get a free quote with Quantum today and see if your business qualifies for Business Owners Policy. </p>
                                 <Button type="block " size="large" className="btn-large text-blue">
-                                          Free Commercial Quote <ArrowRightOutlined />
+                                          Free Commercial Quote <ArrowRightOutlined/>
                                         </Button>
                              </Col>
                              <Col span={12}>
@@ -42,7 +47,84 @@ function App() {
                   </Col>
                 </Row>
               </section>
-          </Content>
+               <section className="section-two">
+                   <div className="section-two-container">
+                        <Row>
+                            <Col span={24}>
+                                  <p className="cl01_title">We are partnered with top rated companies<br/></p>
+                            </Col>
+                        </Row>
+                       <div className="section-two-img-container">
+                             <Row>
+                                <Col xs={{span: 24, offset: 0}} lg={{span: 12, offset: 0}} className="section-two-imgs">
+                                  <img src={travelers} alt="logo1"/>
+                                </Col>
+                                <Col xs={{span: 24, offset: 0}} lg={{span: 9, offset: 3}} className="section-two-imgs">
+                                   <img src={progressive} alt="logo1" className="ml-1"/>
+                                </Col>
+                             </Row>
+                             <Row>
+                                <Col xs={{span: 24, offset: 0}} lg={{span: 12, offset: 0}} className="section-two-imgs">
+                                  <img src={stateAuto} alt="logo1"/>
+                                </Col>
+                                <Col xs={{span: 24, offset: 0}} lg={{span: 10, offset: 2}} className="section-two-imgs">
+                                   <img src={farmers} alt="logo1"/>
+                                </Col>
+                             </Row>
+                       </div>
+                   </div>
+               </section>
+              <section className="container">
+                  <Row>
+                      <Col span={24}>
+                      <h1 className="font-weight-bold ml-md-5">Business insurance</h1>
+                      </Col>
+                  </Row>
+                  <Row>
+                      <Col xs={{span: 24}} lg={{span: 12}}>
+                      <p>A business owners policy includes protection for all major property and liability risk in one package. Offers protection to business owners against property damage, peril, business interruption, and liability. Get a free quote with Quantum Assurance today and see if your business qualifies for Business Owners Policy. </p>
+                          <p>These policies are better suited for smaller businesses as they have more personal financial exposure in the event of a loss. As a business owner, you must ensure that all aspects of your business are covered. From property damage to legal liabilities to employee-related risk.</p>
+                          <p>At Quantum, we offer the best coverage available for your business. Get your free quote today and check out what would be the best fit for your business.</p>
+                      <Button type="block " size="large" className="btn-large text-white bg-blue">
+                                          Start Your Free Qoute <ArrowRightOutlined/>
+                                        </Button>
+                      </Col>
+                      <Col xs={{span: 24}} lg={{span: 12}}>
+                      <div className="service-main-image business">
+                      </div>
+                      </Col>
+                  </Row>
+
+              </section>
+              <section className="container  my-3">
+                  <Row>
+                      <Col span={24}>
+                      <h3 className="mx-auto text-center">You should consider a Business Owners Policy if you:</h3>
+                      </Col>
+                  </Row>
+                  <Row className="mt-3">
+                      <Col xs={{span: 24}} lg={{span: 12}}>
+                    <ul>
+                        <li className="list-item-2"><h6 className="h6-heading list">Need general liability insurance coverage</h6></li>
+                        <li className="list-item-2"><h6 className="h6-heading list">If you have business equipment that you want to make sure is protected</h6></li>
+                    </ul>
+                      </Col>
+                      <Col xs={{span: 24}} lg={{span: 12}}>
+                    <ul>
+                        <li className="list-item-2"><h6 className="h6-heading list">If you happen to own the building that you work at </h6></li>
+                        <li className="list-item-2"><h6 className="h6-heading list">If you have employees that you don’t consider to be trustworthy</h6></li>
+                    </ul>
+                      </Col>
+                  </Row>
+                  <Row className="text-center">
+                      <Col span={24}>
+                          <Button type="block " size="large" className="btn-large text-white bg-blue">
+                                          Start Your Free Qoute <ArrowRightOutlined/>
+                                        </Button>
+                      </Col>
+                  </Row>
+              </section>
+              </Content>
           <Footer className="footer">
                 <div className="footer-container">
                     <div className="footer-top-wrap-2">
